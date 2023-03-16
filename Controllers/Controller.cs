b871da42fb4329e14f1d44430d0b45fe949248d7
Edoardo Ubaldi <Edoubba@gmail.com>
2023-03-16 08:13:30 +0100

    GetPariDispari

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -3,8 +3,18 @@
 namespace AS2223_4G_INF_UbaldiEdoardo_API.Controllers
 {
 
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
     public class Controller : Microsoft.AspNetCore.Mvc.Controller
     {
-        
+        [HttpGet("GetPariDispari")]
+        public JsonResult GetPariDispari(int n1)
+        {
+            if (n1 % 2 == 0) {
+                return Json(new { output = "Pari", status = "OK" });
+            }
+            return Json(new { output = "Dispari", status = "OK" });
+        }
     }
 }
