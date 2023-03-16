ec73f3ece272cb339cdf75d790517aad9b91933a
Edoardo Ubaldi <Edoubba@gmail.com>
2023-03-16 08:03:58 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/Controller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_UbaldiEdoardo_API.Controllers
+{
+
+    public class Controller : Microsoft.AspNetCore.Mvc.Controller
+    {
+        
+    }
+}
